36c06c401e7baa34d402ce90b7c4ce32111a7f40
hazratshahjalal <hazratshahjalal503@gmail.com>
2023-05-03 00:07:16 +0600

    chefs data added successfully

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 
+const chefs = require('./chefs.json')
+
 const app = express();
 const port = 5400
 
-app.get("/", (req, res) => {
-  res.send("hi there")
+// chefs data
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
 })
+
+app.get('/chefs/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log('I need more phone from id:', id)
+  const chef = chefs.find(chef => chef.id === id) || {};
+  res.send(chef)
+})
+
+// recipes data
+
 
 app.listen(port, () => {
   console.log(`server running port ${port}`)
